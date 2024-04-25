d7d6c21bf9c39bc55be1299a2758939f636d46cd
Prathamesh Chougale <prathamesh17170@gmail.com>
2024-04-25 09:28:38 +0530

    corrected the code

--- examination/register.php
+++ examination/register.php
@@ -5,9 +5,9 @@ $name = $_POST['username'];
 $email = $_POST['useremail'];
 $password = $_POST['userpass'];
 
-$sql = "INSERT INTO register VALUES (3,'$name','$email','$password')";
+$sql = "INSERT INTO register VALUES (4,'$name','$email','$password')";
 if($conn->query($sql)===TRUE){
-    header('location:login.php');
+    header('location:login.html');
 }
 else{
     echo  "Error".$sql.'<br>'.$conn->error;
